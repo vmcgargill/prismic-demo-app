72e8620385abeac75a9cfa33c84dd609a88d9e9f
Vincent McGargill <vincentmcgargill@gmail.com>
2021-01-06 20:04:02 -0700

    Updating README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import Prismic from '@prismicio/client'
+import { Date, Link, RichText } from 'prismic-reactjs'
 import './App.css';
 
 function App() {
+  const [home, setHomePage] = React.useState(null);
+  const [automobiles, seAutomobiles] = React.useState([]);
+  
+  React.useEffect(() => {
+    const apiEndpoint = 'https://the-demo-repository.cdn.prismic.io/api/v2'
+    const Client = Prismic.client(apiEndpoint)
+    const fetchData = async () => {
+      const response = await Client.query(
+        Prismic.Predicates.at('document.type', 'home_page')
+      )
+      if (response) {
+        console.log(response)
+        setHomePage(response.results[0])
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      {
+        home ? (
+          <div>
+            <h1>{RichText.asText(home.data.home_page)}</h1>
+            <RichText render={home.data.description} />
+            <img alt='cover' src={home.data.picture.url} class="coverIMG" />
+          </div>
+        ) : <div>No content</div>
+      }
+    </React.Fragment>
+  )
 }
 
 export default App;
